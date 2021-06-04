e10500a61d6428983b14a11b6c67c78db40f658a
LiketheCity <sydney.n.ruffin@gmail.com>
2021-06-04 03:27:40 +0000

    Completed assignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,24 +3,37 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = "1) Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
+let questions = ["2) True or false: 5 kilometer == 5000 meters", "3) (5+3)/2*10 = ?", "4) Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2?", "5) What is the minimum crew size for the ISS?"];
+let correctAnswers = ["true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question("What is your full name?: ")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+candidateAnswer = input.question("1) Who was the first American woman in space?")
+console.log("Your answer: " + candidateAnswer + "\n" + " Correct Answer: Sally Ride" + "\n\n")
 
+candidateAnswer = input.question("2) True or false: 5 kilometer == 5000 meters?")
+console.log("Your answer: " + candidateAnswers + "\n" + " Correct Answer: true" + "\n\n")
+
+candidateAnswer = input.question("3) (5+3)/2*10 = ?")
+console.log("Your answer: " + candidateAnswers + "\n" + " Correct Answer: 40" + "\n\n")
+
+candidateAnswer = input.question("4) Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2?")
+console.log("Your answer: " + candidateAnswers + "\n" + " Correct Answer: Trajectory" + "\n\n")
+
+candidateAnswer = input.question("5) What is the minimum crew size for the ISS?")
+console.log("Your answer: " + candidateAnswers + "\n" + " Correct Answer: 3" + "\n\n")
 
 }
 
@@ -38,8 +51,10 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log("Candiate Name: " + candidateName);
+
   askQuestion();
+  
   gradeQuiz(this.candidateAnswers);
 }
 
