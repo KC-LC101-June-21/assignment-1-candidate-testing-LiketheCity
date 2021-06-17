e57831843ee04ec5bc74212fdceda6327716f0d8
LiketheCity <sydney.n.ruffin@gmail.com>
2021-06-17 18:33:45 +0000

    • Made it so there was a status update based on the grade • Started to work on how to get a grade

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,11 +5,11 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "1) Who was the first American woman in space? ";
-let correctAnswer = "sally ride";
-let candidateAnswer = "";
-let questions = ["2) True or false: 5 kilometer == 5000 meters", "3) (5+3)/2*10 = ?", "4) Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2?", "5) What is the minimum crew size for the ISS?"];
-let correctAnswers = ["true", "40", "Trajectory", "3"];
+// let question = "1) Who was the first American woman in space? ";
+// let correctAnswer = "sally ride";
+// let candidateAnswer = "";
+let questions = ["1) Who was the first American woman in space? ", "2) True or false: 5 kilometer == 5000 meters", "3) (5+3)/2*10 = ?", "4) Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2?", "5) What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
 
@@ -20,8 +20,8 @@ candidateName = input.question("What is your full name?: ")
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question("1) Who was the first American woman in space? ")
-console.log("Your answer: " + candidateAnswer + "\n" + " Correct Answer: Sally Ride" + "\n\n")
+candidateAnswers = input.question("1) Who was the first American woman in space? ")
+console.log("Your answer: " + candidateAnswers + "\n" + " Correct Answer: Sally Ride" + "\n\n")
 
 candidateAnswers = input.question("2) True or false: 5 kilometer == 5000 meters? ")
 console.log("Your answer: " + candidateAnswers + "\n" + " Correct Answer: true" + "\n\n")
@@ -41,17 +41,30 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-
-  let grade;
   
+  // let pointsEarned = questions.length - wrongAnswers;
+  // let correctAnswers = candidateAnswers.includes(correctAnswers);
+  // console.log(correctAnswers);
+  // let wrongAnswersTotal = wrongAnswers.push.(candidateAnswers);
+  // console.log(wrongAnswersTotal.length);
+
+  let grade = (3 / questions.length) * 100;
+
+  console.log(`>>> Overall Grade: ${grade}% (Blank of 5 responses correct)<<<`);
+
+  if (grade <= 59) {
+    console.log(">>> Status: FAILED <<<");
+  } else {
+    console.log(">>> Status: PASSED <<<");
+  }
 
   return grade;
-}
+} 
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Candiate Name: " + candidateName);
+  console.log("Candidate Name: " + candidateName);
 
   askQuestion();
   
@@ -62,9 +75,9 @@ function runProgram() {
 // And don't change these or your program will not run as expected //
 module.exports = {
   candidateName: candidateName,
-  question: question,
-  correctAnswer: correctAnswer,
-  candidateAnswer: candidateAnswer,
+  // question: question,
+  // correctAnswer: correctAnswer,
+  // candidateAnswer: candidateAnswer,
   questions: questions,
   correctAnswers: correctAnswers,
   candidateAnswers: candidateAnswers,
