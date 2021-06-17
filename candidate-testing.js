f800fb9880852ca8315e82c2f991535af627c806
LiketheCity <sydney.n.ruffin@gmail.com>
2021-06-17 23:12:09 +0000

    • added for loop

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,10 +7,10 @@ let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 // let question = "1) Who was the first American woman in space? ";
 // let correctAnswer = "sally ride";
-// let candidateAnswer = "";
+let candidateAnswers = "";
 let questions = ["1) Who was the first American woman in space? ", "2) True or false: 5 kilometer == 5000 meters", "3) (5+3)/2*10 = ?", "4) Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2?", "5) What is the minimum crew size for the ISS?"];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let candidateAnswers = [];
+let collectedAnswers = [];
 
 
 function askForName() {
@@ -21,41 +21,54 @@ candidateName = input.question("What is your full name?: ")
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 candidateAnswers = input.question("1) Who was the first American woman in space? ")
+collectedAnswers.push(candidateAnswers);
 console.log("Your answer: " + candidateAnswers + "\n" + " Correct Answer: Sally Ride" + "\n\n")
 
+
 candidateAnswers = input.question("2) True or false: 5 kilometer == 5000 meters? ")
+collectedAnswers.push(candidateAnswers);
 console.log("Your answer: " + candidateAnswers + "\n" + " Correct Answer: true" + "\n\n")
 
 candidateAnswers = input.question("3) (5+3)/2*10 = ? ")
+collectedAnswers.push(candidateAnswers);
 console.log("Your answer: " + candidateAnswers + "\n" + " Correct Answer: 40" + "\n\n")
 
 candidateAnswers = input.question("4) Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2? ")
+collectedAnswers.push(candidateAnswers);
 console.log("Your answer: " + candidateAnswers + "\n" + " Correct Answer: Trajectory" + "\n\n")
 
 candidateAnswers = input.question("5) What is the minimum crew size for the ISS? ")
+collectedAnswers.push(candidateAnswers);
 console.log("Your answer: " + candidateAnswers + "\n" + " Correct Answer: 3" + "\n\n")
 
+console.log(collectedAnswers);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  
+  let totalCorrect = 0;
+  console.log(totalCorrect);
 
   
-  // let pointsEarned = questions.length - wrongAnswers;
-  // let correctAnswers = candidateAnswers.includes(correctAnswers);
-  // console.log(correctAnswers);
-  // let wrongAnswersTotal = wrongAnswers.push.(candidateAnswers);
-  // console.log(wrongAnswersTotal.length);
 
-  let grade = (3 / questions.length) * 100;
+  for (i = 0; i < questions.length; i++) {
+   if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
+     totalCorrect += 1;
+   }
+    console.log(totalCorrect);
+  }
 
-  console.log(`>>> Overall Grade: ${grade}% (Blank of 5 responses correct)<<<`);
 
-  if (grade <= 59) {
-    console.log(">>> Status: FAILED <<<");
-  } else {
+  let grade = (totalCorrect.length / questions.length) * 100;
+
+  console.log(`>>> Overall Grade: ${grade}% (${totalCorrect} of 5 responses correct)<<<`);
+
+  if (grade => 60) {
     console.log(">>> Status: PASSED <<<");
+  } else {
+    console.log(">>> Status: FAILED <<<");
   }
 
   return grade;
